e358d30488ab50cd7457caedd62436f2a96c01aa
Nathan Reuter Godinho <nathan0reuter@gmail.com>
2017-04-22 10:39:40 -0300

    adding i-robot

--- src/player.js
+++ src/player.js
@@ -11,8 +11,99 @@
         return Math.ceil(Math.random() * maxSticks);
     };
 
-    var findBestBet = function (players) {
+    var findBestBet = function (players, playersBets, totalInGameSticks) {
+        var player = this;
+        var parentNode = {
+            isParent: true,
+            children: []
+        };
+        var nodesCount = 0;
 
+        var createNode = function (parentNode, lastNodeSticks) {
+            var newNode = {
+                id: nodesCount,
+                parentNode: parentNode,
+                children: []
+            };
+
+            if(lastNodeSticks < player.inHandSticks){
+                newNode.bet = player.inHandSticks;
+            } else {
+                newNode.bet = lastNodeSticks + 1;
+            }
+
+            nodesCount++;
+
+            return newNode;
+        };
+
+        var populateVisitedList = function (playerBets) {
+            if (!playerBets.length) {
+                return [];
+            } else if (playerBets.length === 1) {
+                return [createNode(parentNode, playerBets[0])]
+            }
+
+            var visitedNodes = [];
+
+            _.reduce(playersBets, function (last, current) {
+                var parent = createNode(parentNode, last);
+                visitedNodes.push(parent);
+
+                return createNode(parent, current);
+            });
+
+            return visitedNodes;
+        };
+
+
+        var calculateGuess = function (nodo) {
+            var diference = 0;
+
+            _.forEach(visitedNodes, function (visited, index) {
+                var diff = players[index + 1].qtdMaxPalitos - nodo.bet;
+
+                if (diff > 0) {
+                    diference += diff;
+                }
+            });
+
+            if (nodo.bet < player.inHandSticks || nodo.bet > totalInGameSticks - diference) {
+                return 0;
+            }
+
+            debugger;
+
+            var guess = ((((totalInGameSticks - diference) - player.totalSticks) / players.length) / 2 )
+                    * players.length + player.totalSticks;
+
+            return guess * 10 - (Math.abs(nodo.bet - guess));
+
+        };
+
+        var visitedNodes = populateVisitedList(playersBets);
+        var actualNode = createNode(parentNode, 0);
+        var nextNode = createNode(parentNode, actualNode.bet);
+
+        parentNode.children.push(actualNode);
+        parentNode.children.push(nextNode);
+
+        var actualNodeGrade = calculateGuess(actualNode);
+        var nextNodeGrade = calculateGuess(nextNode);
+
+        console.log('actualNodeGrade', actualNodeGrade);
+        console.log('nextNodeGrade', nextNodeGrade);
+
+        while(actualNodeGrade < nextNodeGrade){
+            actualNode = nextNode;
+            actualNodeGrade = nextNodeGrade;
+            nextNode = createNode(parentNode, nextNode.bet);
+            parentNode.children.push(nextNode);
+            nextNodeGrade = calculateGuess(nextNode);
+        }
+
+        console.log('Final Grade', actualNode.bet);
+        return actualNode.bet;
     };
 
     var Player = function (id) {
@@ -39,10 +130,9 @@
     };
 
     Player.prototype.bet = function (totalSticksInGame, players, playersBets) {
-        // findBestBet(players);
-        console.log(playersBets);
+        return findBestBet.call(this, players, playersBets, totalSticksInGame);
 
-        return getPlayerSticks(totalSticksInGame);
+        // return getPlayerSticks(totalSticksInGame);
     };
 
     module.exports = Player;
